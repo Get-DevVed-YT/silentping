eba64cbea4a8b3a622902f23211c38476dbdac81
wispkoe <>
2021-05-28 03:39:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,324 @@
+var msgs = [{
+  "name": "Server",
+  "text": "WT-SilentPing Vercel Archive Backup Server booted."
+},
+{
+  "name": "Server",
+  "text": "Finding archive from December 23, 2012"
+},{
+    "name": "Server",
+  "text": "Archive found! Loading history..."
+},{
+  "name": "Jack89",
+  "text": "Yo what's good fellas"
+},{
+  "name": "SPMemer",
+  "text": "Oi mates"
+},{
+  "name": "Jack89",
+  "text": "Huh, normally SilentPing would be pretty active by now..."
+},{
+  "name": "SPMemer",
+  "text": "It's the holidays, maybe people have other better things to do then waste their time here."
+},{
+  "name": "Jack89",
+  "text": "Good point @SPMemer"
+},{
+  "name": "LolKitties",
+  "text": "Hi im new"
+},{
+  "name": "SPMemer",
+  "text": "Hi new im SPMemer"
+},{
+  "name": "Jack89",
+  "text": "That's not funny anymore, @SPMemer"
+},{
+  "name": "LolKitties",
+  "text": "It was kind of funny lol"
+},{
+  "name": "LolKitties",
+  "text": "So how does this work?"
+},{
+  "name": "Jack89",
+  "text": "I think you pretty much got the hang of it."
+},{
+  "name": "<b>Dev Bot</b>",
+  "text": "Merry early Christmas to everyone from us at SilentPing!"
+},{
+  "name": "SPMemer",
+  "text": "Shut up Dev Bot, no one cares."
+},{
+  "name": "Jack89",
+  "text": "lmao"
+},{
+  "name": "Jack89",
+  "text": "It was considerate  of them to say it."
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "hoi"
+},{
+  "name": "SPMemer",
+  "text": "Did you fall asleep on your keyboard when writing your username?"
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "Yes... by mistake :P"
+},{
+  "name": "SPMemer",
+  "text": "LOLOLOL"
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "So what are you guys doing?"
+},{
+  "name": "Jack89",
+  "text": "Just talking here"
+},{
+  "name": "SPMemer",
+  "text": "No duh"
+},{
+  "name": "Jack89",
+  "text": "So, got any plans for Christmas?"
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "Well, im planning to go to the Bethlehem this Christmas"
+},{
+  "name": "SPMemer",
+  "text": "Im not going anywhere, just playing Pixelshooty."
+},{
+  "name": "LolKitties",
+  "text": "What is Pixelshooty?"
+},{
+  "name": "SPMemer",
+  "text": "Only the best game EVER. <a href=\"pixelshooty.html\" target=\"_blank\">pixelshooty.html</a>"
+},{
+  "name": "LolKitties",
+  "text": "I'll try it out later"
+},{
+  "name": "Jack89",
+  "text": "SilentPing sucks at game design, they shouldn't try to make any more games."
+},{
+  "name": "<i>Anonymous Chatter</i>",
+  "text": "Hi"
+},{
+  "name": "Jack89",
+  "text": "Lol an anonymous chatter. Couldn't even bother to sign up."
+},{
+  "name": "SPMemer",
+  "text": "An even bigger noob than @LolKitties"
+},{
+  "name": "LolKitties",
+  "text": "Thats not nice!"
+},{
+  "name": "<i>Anonymous Chatter<i>",
+  "text": "I am not new to this. Would you guys like to play a game?"
+},{
+  "name": "SPMemer",
+  "text": "Sure. Pixelshooty tournament?"
+},{
+  "name": "<i>Anonymous Chatter</i>",
+  "text": "I had... something different in mind."
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "What is the game?"
+},{
+  "name": "<i>Anonymous Chatter</i>",
+  "text": "I will send each of you corrupted text. If you can decode it, you win."
+},{
+  "name": "Jack89",
+  "text": "Meh, Whatever."
+},{
+  "name": "<i>Anonymous Chatter</i>",
+  "text": "@Jack89 01011001 01101111 01110101 00100000 01100001 01100011 01110100 01110101 01100001 01101100 01101100 01111001 00100000 01100100 01100101 01100011 01101111 01100100 01100101 01100100 00100000 01110100 01101000 01101001 01110011 00111111"
+},{
+  "name": "<i>Anonymous Chatter</i>",
+  "text": "@LolKitties 01001111 01101000 00100000 01101000 01101111 01110111 00100000 01001001 00100000 01110000 01101001 01110100 01111001 00100000 01111001 01101111 01110101 00101110"
+},{
+  "name": "<i>Anonymous Chatter</i>",
+  "text": "@SPMemer 01000010 01100101 01100011 01100001 01110101 01110011 01100101 00100000 01101111 01100110 00100000 01110100 01101000 01101001 01110011 00001010"
+},{
+  "name": "<i>Anonymous Chatter</i>",
+  "text": "@Kkwjosodkjsljlckodjojcc 01001001 01110100 00100000 01110111 01101001 01101100 01101100 00100000 01100001 01101100 01101100 00100000 01100010 01100101 00100000 01100111 01101111 01101110 01100101"
+},{
+  "name": "Jack89",
+  "text": "This is just binary. Use a binary-to-text converter."
+},{
+  "name": "Jack89",
+  "text": "\"You actually decoded this?\""
+},{
+  "name": "LolKitties",
+  "text": "I got \"Oh how I pity you.\""
+},{
+  "name": "SPMemer",
+  "text": "\"Because of this\""
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "\"It will all be gone\"?"
+},{
+  "name": "SPMemer",
+  "text": "What the hell?"
+},{
+  "name": "<i>Anonymous Chatter</i>",
+  "text": "You have all made a mistake. Now you will pay."
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "LOL trolls be like oOOooOh spOoOoOoky"
+},{
+  "name": "Server",
+  "text": "<i>Anonymous Chatter</i> has left the chat."
+},{
+  "name": "Jack89",
+  "text": "Well that was weird"
+},{
+  "name": "LolKitties",
+  "text": "Can I admit something?"
+},{
+  "name": "LolKitties",
+  "text": "I've been quite scared of this paranormal stuff, especially considering I think I'm being stalked."
+},{
+  "name": "Jack89",
+  "text": "You're being stalked??"
+},{
+  "name": "LolKitties",
+  "text": "I believe so. I see some unknown person in my yard from time to time."
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "Have you reported this to the police?"
+},{
+  "name": "LolKitties",
+  "text": "The police don't believe me. I've tried reporting multiple times."
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "ok, what the hell. I just got a call from an unknown number."
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "Whatever they're saying, the audio is corrupted."
+},{
+  "name": "SPMemer",
+  "text": "Are you trolling now?"
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "No, i swear to God. I should've recorded it."
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "Ok what the f***. <a href='Photo-1j83j9fje83jpf9l2kqj8di.png' target='_blank'>New image.</a>"
+},{
+  "name": "Jack89",
+  "text": "What the actual hell"
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "CRAP IT'S ENTERING MY HOUSE"
+},{
+  "name": "Jack89",
+  "text": "You need to hide."
+},{
+  "name":"Kkwjosodkjsljlckodjojcc",
+  "text":"Ok I'm hiding in the basement"
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "F*** IT'S COMING DOWN"
+},{
+  "name": "SPMemer",
+  "text": "oh my God"
+},{
+  "name": "Kkwjosodkjsljlckodjojcc",
+  "text": "IT SAW ME"
+},{
+  "name": "Jack89",
+  "text": "OH GOD"
+},{
+  "name": "Server",
+  "text": "Kkwjosodkjsljlckodjojcc has left the chat."
+},{
+  "name": "SPMemer",
+  "text": "..."
+},{
+  "name": "Jack89",
+  "text": "Did they just-"
+},{
+  "name": "LolKitties",
+  "text": "OH NO NOT AGAIN!"
+},{
+  "name": "Jack89",
+  "text": "What happened?"
+},{
+  "name": "LolKitties",
+  "text": "MY JOURNAL! WHOEVER HAS BEEN STALKING ME VANDALIZED IT OVER THE PAST FEW MONTHS AND DID IT AGAIN!"
+},{
+  "name": "Jack89",
+  "text": "You showed this to the police and they didn't believe you? What a law enforcement we have."
+},{
+  "name": "LolKitties",
+  "text": "MY STALKER IS OUTSIDE MY HOUSE AGAIN"
+},{
+  "name": "SPMemer",
+  "text": "Run..."
+},{
+  "name": "LolKitties",
+  "text": "AM I GONNA DIE TOO?!?!"
+},{
+  "name": "LolKitties",
+  "text": "IT'S ENTERED MY HOUSE... IT'S FOUND ME!"
+},{
+  "name": "Server",
+  "text": "LolKitties has left the chat."
+},{
+  "name": "SPMemer",
+  "text": "...we just witnessed two murders on a chat site"
+},{
+  "name": "Jack89",
+  "text": "Let's not add ourselves to the list! We need to run."
+},{
+  "name": "SPMemer",
+  "text": "I'm calling a cab and going to the airport... I'm getting out of here!"
+},{
+  "name": "Jack89",
+  "text": "Damnit, I can't afford plane tickets... I'm going to the police"
+},{
+  "name": "SPMemer",
+  "text": "Will they really believe that two murders happened on a chat site? First off, those guys might not live near you, so they can't prove. Second, they'll think it's a load of crap anyway."
+},{
+  "name": "Jack89",
+  "text": "Ah, you're right. I'm just gonna run out of here. Idc where, just somewhere."
+},{
+  "name": "SPMemer", 
+  "text": "The cab came, I'm getting in."
+},{
+  "name": "SPMemer",
+  "text": "S*** THE DRIVER IS THE GUY"
+},{
+  "name": "Server",
+  "text": "SPMemer has left the chat."
+},{
+  "name": "Jack89",
+  "text": "OH DAMN IT'S THERE"
+},{
+  "name": "Server",
+  "text": "Jack89 has left the chat."
+},{
+  "name": "<i>Anonymous Chatter</i>",
+  "text": "\"And then there were none.\""
+},{
+  "name": "Server",
+  "text": "ACTION: Archive and shutdown. Archiving... shutting down..."
+},{
+  "name": "Server",
+  "text": "<b>END OF ARCHIVE</b>"
+}];
+
+const timer = ms => new Promise(res => setTimeout(res, ms));
+
+
+async function loop() {
+  for(var i = 0; i < msgs.length; i += 1) {
+  var obj = msgs[i];
+  console.log(obj);
+ x = document.getElementById("messages");
+ let msg = document.createElement("span");
+ let br = document.createElement("br")
+ msg.innerHTML = obj.name + " : " + obj.text;
+ x.append(msg);
+ x.append(br);
+  await timer(3000);
+}
+}
+
+loop();
